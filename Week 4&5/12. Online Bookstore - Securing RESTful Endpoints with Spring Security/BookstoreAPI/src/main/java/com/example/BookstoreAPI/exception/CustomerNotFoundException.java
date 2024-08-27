4cbfac769fbcbd2669595c3237e3eb5a44be04a3
Meghadri Gautami <118245644+Gautami1103@users.noreply.github.com>
2024-08-27 22:43:38 +0530

    Create CustomerNotFoundException.java

---
+++ Week 4&5/12. Online Bookstore - Securing RESTful Endpoints with Spring Security/BookstoreAPI/src/main/java/com/example/BookstoreAPI/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.book.BookstoreAPI.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
