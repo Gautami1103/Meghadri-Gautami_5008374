50367bf785aeac72af78d18c4f16da47be1e796a
Meghadri Gautami <118245644+Gautami1103@users.noreply.github.com>
2024-08-27 12:14:55 +0530

    Update BookstoreApiApplication.java

--- Week 4&5/10. Online Bookstore - Configuring Content Negotiation/BookstoreAPI/src/main/java/com/example/BookstoreAPI/BookstoreApiApplication.java
+++ Week 4&5/10. Online Bookstore - Configuring Content Negotiation/BookstoreAPI/src/main/java/com/example/BookstoreAPI/BookstoreApiApplication.java
@@ -1,13 +1,13 @@
-package com.example.ex10;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Ex10Application {
+public class BookstoreApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ex10Application.class, args);
+		SpringApplication.run(BookstoreApiApplication.class, args);
 	}
 
 }
